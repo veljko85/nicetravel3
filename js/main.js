4363e3701a5f20192dc34d6dc36eb994b334ac0b
Velibor Markovic <veljko85@users.noreply.github.com>
2019-03-02 20:58:23 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ $('.carousel').carousel();
 
 var hamburger = document.getElementById('hamburgerButton');
 
+
 hamburgerButton.onclick = () => {
 	var line2 = document.getElementById("secondLine").style.display;
-
+	var navWidth = document.getElementById('nav').style.width;
 
 	if (line2 == "none") {
 		document.getElementById("firstLine").classList.remove("change1");
@@ -15,6 +16,11 @@ hamburgerButton.onclick = () => {
 		document.getElementById("firstLine").classList.add("change1");
 		document.getElementById("secondLine").style.display = "none";
 		document.getElementById("thirdLine").classList.add("change2");
+	}
+	if (navWidth == "70%") {
+		document.getElementById("nav").style.width = "0";
+	} else	{
+		document.getElementById("nav").style.width = "70%";
 	}
 	
 }
